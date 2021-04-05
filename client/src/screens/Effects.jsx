7e51b50f765a5cd5b57d1ecc0837a1ce76d8dd0b
Wifey <wifey@Adrians-MacBook-Air.local>
2021-04-05 21:59:49 +0100

    formatting code

--- client/src/screens/Effects.jsx
+++ client/src/screens/Effects.jsx
@@ -1,34 +1,33 @@
-import { getAllEffects } from '../services/effects';
-import React, { useState, useEffect } from 'react';
-import './Effects.css';
+import { getAllEffects } from "../services/effects";
+import React, { useState, useEffect } from "react";
+import "./Effects.css";
 
 export default function Effects() {
   const [effects, setEffects] = useState([]);
 
   useEffect(() => {
     const fetchEffects = async () => {
-      const effectData = await getAllEffects()
+      const effectData = await getAllEffects();
       setEffects(effectData);
-    }
+    };
     fetchEffects();
-  }, [])
+  }, []);
 
   return (
-    <div className='about'>
-      <p className='title'>Here's some food for thought...</p>
-      <p className='intro'>
-        If you've ever experienced a food coma, you probably concluded that certain types of food have an effect on your body and its functions. Here are a few of the good and bad ways that certain food items can change your physical state.
+    <div className="about">
+      <p className="title">Here's some food for thought...</p>
+      <p className="intro">
+        If you've ever experienced a food coma, you probably concluded that
+        certain types of food have an effect on your body and its functions.
+        Here are a few of the good and bad ways that certain food items can
+        change your physical state.
       </p>
-
-      <p className='body'>How food can affect you:</p>
-      {
-        effects?.map(effect => (
-          <p key={effect.id}>{effect.content}</p>
-        ))
-      }
-    
-      Want to see which specific foods cause these effects? Open up that hamburger menu and go to Foods to get started!
-      
+      <p className="body">How food can affect you:</p>
+      {effects?.map((effect) => (
+        <p key={effect.id}>{effect.content}</p>
+      ))}
+      Want to see which specific foods cause these effects? Open up that
+      hamburger menu and go to Foods to get started!
     </div>
-  )
+  );
 }
