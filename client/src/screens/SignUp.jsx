c5776cb8b7b762cbded10732b6331f77148dc5c4
Wifey <wifey@Adrians-MacBook-Air.local>
2021-04-05 22:04:24 +0100

    added comments for password check

--- client/src/screens/SignUp.jsx
+++ client/src/screens/SignUp.jsx
@@ -1,46 +1,49 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function Register(props) {
   const [message, setMessage] = useState("");
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    password_confirmation: ''
-  })
+    username: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
   const { username, email, password, password_confirmation } = formData;
   const { handleRegister } = props;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   useEffect(() => {
-    if (password === "" || password_confirmation === ""){
+    // adding this useEffect to compare password with password_confirmation values and notify user if successful
+    if (password === "" || password_confirmation === "") {
       setMessage(" ");
     } else if (password !== password_confirmation) {
-      setMessage("Passwords don't match!")
+      setMessage("Passwords don't match!");
     } else if (password === password_confirmation) {
-      setMessage("Passwords match!")
-    } 
-
-  }, [password, password_confirmation])
+      setMessage("Passwords match!");
+    }
+  }, [password, password_confirmation]);
 
   return (
-    <form className='edit-fields, form' onSubmit={(e) => {
-      e.preventDefault();
-      handleRegister(formData);
-    }}>
+    <form
+      className="edit-fields, form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleRegister(formData);
+      }}
+    >
       <h3>Register</h3>
       <label>
         Username:
         <input
-          type='text'
-          name='username'
+          type="text"
+          name="username"
           value={username}
           onChange={handleChange}
         />
@@ -48,35 +51,32 @@ export default function Register(props) {
       <br />
       <label>
         Email:
-        <input
-          type='text'
-          name='email'
-          value={email}
-          onChange={handleChange}
-        />
+        <input type="text" name="email" value={email} onChange={handleChange} />
       </label>
       <br />
       <label>
         Password:
         <input
-          type='password'
-          name='password'
+          type="password"
+          name="password"
           value={password}
           onChange={handleChange}
         />
       </label>
-      <br/>
+      <br />
       <label>
         Confirm Password:
         <input
-          type='password'
-          name='password_confirmation'
+          type="password"
+          name="password_confirmation"
           value={password_confirmation}
           onChange={handleChange}
         />
-      </label><br/>
-      {message}<br/>
+      </label>
+      <br />
+      {message}
+      <br />
       <button>Submit</button>
     </form>
-  )
+  );
 }
